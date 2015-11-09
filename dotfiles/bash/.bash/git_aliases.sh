c62a4bb33e5361c6334e05d73c477295a1f30a33
Ben Moon <guiltydolphin@gmail.com>
2015-11-09 15:54:43 +0000

    Add functions for git rebasing
    These make it easier to preserve commit information when splitting
    commits in a rebase.

--- dotfiles/bash/.bash/git_aliases.sh
+++ dotfiles/bash/.bash/git_aliases.sh
@@ -6,3 +6,22 @@ alias go='git checkout '
 alias gd='git diff '
 alias gk='gitk --all&'
 alias gx='gitx --all'
+
+# Update the GIT_HASH environment variable to the current
+# commit's hash.
+ghash() {
+  export GHASH=$(git log -1 --pretty=format:%H)
+}
+
+# Perform a commit, but preserve hash.
+# For use with git-rebase so that author time-stamps aren't
+# messed up.
+basec() {
+  git commit -c $GHASH
+}
+
+# Reset head, but keep track of the original hash.
+# Use instead of 'git reset HEAD^' when rebasing.
+resh() {
+  ghash && git reset HEAD^
+}
