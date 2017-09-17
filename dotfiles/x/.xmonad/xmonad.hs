c9cc1e281bfb7007b6603326e70a975340dd7eb2
Ben Moon <software@guiltydolphin.com>
2017-09-17 15:36:12 +0100

    Add space for panel dock and tray

--- dotfiles/x/.xmonad/xmonad.hs
+++ dotfiles/x/.xmonad/xmonad.hs
@@ -1,6 +1,8 @@
 import XMonad
+import XMonad.Hooks.ManageDocks
 
-main =
-    xmonad def {
-        modMask = mod4Mask  -- Rebind Mod to the 'Windows' key
-        }
+main = xmonad def
+    { manageHook = manageDocks <+> manageHook def
+    , layoutHook = avoidStruts  $  layoutHook def
+    , modMask    = mod4Mask  -- Rebind Mod to the 'Windows' key
+    }
