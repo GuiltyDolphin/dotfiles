f94eed4b6067e1920d1cb6083f33fc5a1ecb25d2
Ben Moon <software@guiltydolphin.com>
2017-10-09 09:13:39 +0100

    Ensure missing version doesn't halt installation
    When attempting to get the current version for a program, if we are
    unable to do so, ensure we warn the user about this and assume the
    program is up-to-date, rather than halting the entire installation.

--- install/install.pl
+++ install/install.pl
@@ -528,7 +528,7 @@ sub run_config_or_default {
 
 sub is_up_to_date {
     my $program = shift;
-    my $current = run_config_or_default($program, 'version', 'current');
+    my $current = eval { run_config_or_default($program, 'version', 'current') };
     info('could not retrieve current version number, assuming up-to-date')
         and return 1 unless defined $current;
     my $latest  = run_config_or_default($program, 'version', 'latest');
