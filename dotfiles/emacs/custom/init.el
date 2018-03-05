bd7f1e1a16a81fb010ef95299452ec448f5f2ac6
Ben Moon <software@guiltydolphin.com>
2018-03-05 11:14:28 +0000

    Ensure MELPA packages are available

--- dotfiles/emacs/custom/init.el
+++ dotfiles/emacs/custom/init.el
@@ -16,6 +16,15 @@
 
 ;;; Code:
 
+;; Ensure MELPA packages are available
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives (cons "melpa" url) t))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 (org-babel-load-file (locate-user-emacs-file "config.org"))
 
