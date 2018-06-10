e4f71709ab735933ebb601b0f50ce7404120db9f
Ben Moon <software@guiltydolphin.com>
2018-06-10 16:09:12 +0100

    Add more information to init file header

--- dotfiles/emacs/custom/init.el
+++ dotfiles/emacs/custom/init.el
@@ -1,6 +1,11 @@
-;; Emacs Configuration File
-;; Copyright (C) 2015-2018 Ben Moon
+;; init.el --- Emacs Configuration File
 ;;
+;; Copyright (C) 2015-2018 Ben Moon
+;; Author: Ben Moon <software@guiltydolphin.com>
+;; URL: https://github.com/GuiltyDolphin/dotfiles
+;; Git-Repository: git://github.com/GuiltyDolphin/dotfiles.git
+;; Created: 2015-03-01
+
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
@@ -13,6 +18,10 @@
 ;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Configuration files for Emacs.
 
 ;;; Code:
 
