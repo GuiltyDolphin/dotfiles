e377a90a1bcf827372a088b240a128fe3eddbf34
Ben Moon <software@guiltydolphin.com>
2018-07-17 15:22:37 +0100

    Fix FreeMind not displaying correctly with XMonad

--- dotfiles/x/.xmonad/xmonad.hs
+++ dotfiles/x/.xmonad/xmonad.hs
@@ -1,6 +1,8 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+-- to fix freemind not displaying correctly (https://stackoverflow.com/questions/30742662/java-swing-gui-not-displaying-in-xmonad#30742663)
+import XMonad.Hooks.SetWMName (setWMName)
 
 main = xmonad =<< xmobar myConfig
 
@@ -8,5 +10,7 @@ myConfig = def
     { layoutHook = avoidStruts  $  layoutHook def
     , manageHook = manageDocks <+> manageHook def
     , modMask    = mod4Mask  -- Rebind Mod to the 'Windows' key
+      -- to fix freemind not displaying correctly (https://stackoverflow.com/questions/30742662/java-swing-gui-not-displaying-in-xmonad#30742663)
+    , startupHook = setWMName "LG3D"
     , terminal   = "urxvt"
     }
