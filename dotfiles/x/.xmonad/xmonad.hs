51485977208eae4913c1f317bf9f526c2ae4770d
Ben Moon <software@guiltydolphin.com>
2019-03-08 08:24:05 +0000

    Add some workspace bindings for XMonad

--- dotfiles/x/.xmonad/xmonad.hs
+++ dotfiles/x/.xmonad/xmonad.hs
@@ -1,6 +1,12 @@
 import qualified Data.Map as M
 
 import XMonad
+import XMonad.Actions.CycleWS
+    ( nextWS
+    , prevWS
+    , shiftToNext
+    , shiftToPrev
+    )
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 -- to fix freemind not displaying correctly (https://stackoverflow.com/questions/30742662/java-swing-gui-not-displaying-in-xmonad#30742663)
@@ -12,7 +18,12 @@ main = xmonad =<< xmobar myConfig
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 myKeys conf@(XConfig { modMask = mm }) = M.union myKeys' (keys def conf)
     where myKeys' = M.fromList $
-            [ ((mm,               xK_p), spawn "rofi -show run -location 2 -width 100") ]
+            [ ((mm .|. shiftMask, xK_l           ), nextWS)
+            , ((mm .|. shiftMask, xK_h           ), prevWS)
+            , ((mm,               xK_bracketright), shiftToNext >> nextWS)
+            , ((mm,               xK_bracketleft ), shiftToPrev >> prevWS)
+            , ((mm,               xK_p           ), spawn "rofi -show run -location 2 -width 100")
+            ]
 
 myConfig = def
     { keys       = myKeys
