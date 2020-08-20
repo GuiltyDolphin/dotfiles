df99226c1397d51df45a33672a1e8b7022b45ccb
Ben Moon <software@guiltydolphin.com>
2020-08-20 12:25:39 +0100

    Update package-initialize for emacs 27

--- dotfiles/emacs/custom/init.el
+++ dotfiles/emacs/custom/init.el
@@ -34,7 +34,8 @@
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize)
+(when (< emacs-major-version 27)
+  (package-initialize))
 
 ;; set file for custom variables
 (setq custom-file (locate-user-emacs-file ".emacs-custom.el"))
